55008123a0fb58a5cf9c999af95eeaf27f2cf439
Mehemmed Elizade <mehemmedelizade@Mehemmed-MacBook-Air.local>
2023-03-02 00:24:43 +0400

    Repeat

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿ string Repeat(string word, int count)
+{
+    if (count < 1)
+    {
+        return "";
+    }
+    else
+    {
+        string repeatedString = word;
+        for (int i = 1; i < count; i++)
+        {
+            repeatedString += word;
+        }
+        return repeatedString;
+    }
+}
+Console.WriteLine(Repeat("Ha!", 1));
+Console.WriteLine(Repeat("Ha!", 3));
